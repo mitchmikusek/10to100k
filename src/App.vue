6b5b234b8a63c441e299d18173d3e0d4842a3359
Mitch Mikusek <mitch@beewick.com>
2023-04-19 00:24:25 -0700

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+  <div class="main-wrapper">
+    <a href="https://discordapp.com/users/catsensei#3281">
+      <img width="50" src="/discord-mark-white.svg" title="Have Ideas?" alt="catsensei @ discord" />
     </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <a href="https://github.com/mitchmikusek/10to100k">
+      <img width="50" src="/github-mark-white.svg" title="Wanna Help?" alt="mitchmikusek @ github" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.main-wrapper {
+  background-color: rgba(0, 0, 0, .6);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  gap: 50px;
+  position: fixed;
+  right: 10px;
+  bottom: 10px;
+  border-radius: 25px;
 }
 </style>
